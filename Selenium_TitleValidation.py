68f6a5a7e043a9528c67889e06b7c0ce60eb411a
Aashik Afshan A S <aashikrasheed786@gmail.com>
2025-05-27 17:35:37 +0530

    Selenium_TitleValidation.py

--- Selenium_TitleValidation.py
+++ Selenium_TitleValidation.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.firefox.service import Service
 import time
 
-geckodriver_path = r"C:\External\Nanna\Code\sw testing\geckodriver.exe"
+geckodriver_path = r"geckodriver.exe"#Enter Geckodriver Location
 service = Service(geckodriver_path)
 
 driver = webdriver.Firefox(service=service)
