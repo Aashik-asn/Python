e7201fefd21033f8299b0a36f2cdb1bf8471fd9b
Aashik Afshan A S <aashikrasheed786@gmail.com>
2025-12-06 14:50:43 +0530

    optimize

--- encrypt_decrypt.py
+++ encrypt_decrypt.py
@@ -1,21 +1,14 @@
-def encrypt(text,shift):
+def cypher(text,shift):
     s=""
     for i in text:
         s+=chr(ord(i)+shift)
     print(s)
-def decrypt(text,shift):
-    s=""
-    for i in text:
-        s+=chr(ord(i)-shift)
-    print(s)
-
 while True:
     text=input("Enter text: ")
     shift=int(input("Enter shift value in integer: "))
     flag=input("Enter \"encrypt\" or \"decrypt\": ").lower()
-    if flag=="encrypt":
-        encrypt(text,shift)
-    elif flag=="decrypt":
-        decrypt(text,shift)
+    if flag=='decrypt':
+        shift*=-1
+    cypher(text,shift)
     if(input("Do you want to continue (Y/N)? ").lower()=='n'):
         break
